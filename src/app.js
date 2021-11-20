43fbfe3b830eb6dbc570334c865b74de118e3c8d
Carla-Margara <90868430+Carla-Margara@users.noreply.github.com>
2021-11-20 21:44:27 +0000

    Updated index.html, app.js and style.css to eliminate Fahrenheit conversion in #main-temp

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
@@ -58,7 +58,7 @@ function displayForecast(response) {
           alt=""
           width="44"
         />
-        <div class="nextfive-forecast-temperatures">
+        <div class="card-text nextfive-forecast-temperatures">
           <span class="nextfive-forecast-temperatures-max"> ${Math.round(
             forecastDay.temp.max
           )}° </span>
@@ -117,32 +117,14 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  //remove the active class the celsius link
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let temperatureElement = document.querySelector("#main-temp");
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
 function displayCelsiusTemperature(event) {
   event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
+
   let temperatureElement = document.querySelector("#main-temp");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-let celsiusTemperature = null;
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Madrid");
